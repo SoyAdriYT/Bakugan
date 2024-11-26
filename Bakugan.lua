b404dca66c37837157212c03cc555525a71a854a
G-MX2 <127357342+SoyAdriYT@users.noreply.github.com>
2024-11-25 22:46:13 -0600

    Update Bakugan.lua

--- Bakugan.lua
+++ Bakugan.lua
@@ -1,1 +1,10 @@
+local success, response = pcall(function()
+    return game:HttpGet("https://raw.githubusercontent.com/SoyAdriYT/Bakugan/refs/heads/main/Games/Blade%20Ball.lua")
+end)
 
+if success and response then
+    local loadFunction = loadstring(response)
+    if loadFunction then
+        loadFunction()
+    end
+end
