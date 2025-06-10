1560071281c82cdf1d4ce32bb1d7bbf99b686f17
G-MX2 <127357342+SoyAdriYT@users.noreply.github.com>
2025-06-09 23:09:57 -0600

    Update Blade Ball.lua

--- Games/Blade Ball.lua
+++ Games/Blade Ball.lua
@@ -1,3 +1,3 @@
--- If you read this is that I gave up I will not do scripts since Alexis left I am nothing now I will leave this Script which is one of the best I have found I think I will now create a Hub script where scripts are for several games goodbye 
+-- If you read this is that I gave up I will not do scripts since Alexis left I am nothing now I will leave this Script which is one of the best I have found I think I will now create a Hub script where scripts are for several games This scirpt is not it was in case you are wondering goodbye los extrañare mucho pero no volveré le dedicare tiempoa mi vida personal y no me envíen mensaje ni nada que no responderé 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/refs/heads/main/Games/Blade%20Ball.lua"))()
